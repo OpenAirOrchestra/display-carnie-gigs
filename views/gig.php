bd866f01fac0cfec84f3c5ab692768071d61b415
Darryl Fuller <darryl@tzap.ca>
2018-12-19 21:01:19 -0800

    first light of day on short gigs

--- views/gig.php
+++ views/gig.php
@@ -15,7 +15,7 @@ class displayCarnieGigView {
 		$output = '';
 
 		foreach ($gigs as $gig) {
-			$output = $output . $this->shortGig($gig) . '<br/>';
+			$output .= $this->shortGig($gig);
 		}
 
 		return $output;
@@ -25,7 +25,41 @@ class displayCarnieGigView {
 	 * Render a single short gig 
 	 */
 	function shortGig($gig) {
-		return "Short Gig ";
+		$output .= "<a href='javascript:;' class='expand_button' onclick='toggle_parent_collapsed(event);'>&nbsp;&nbsp;&nbsp;&nbsp;</a> <span class='date'>"; 
+
+		if (strtotime($gig['date']) > 0) {
+			$output .=  date('j M Y', strtotime($gig['date']));
+			$output .= ': ';
+		} 
+
+		if ((strncasecmp($gig['url'], 'http://', 7) == 0) ||
+		    (strncasecmp($gig['url'], 'https://', 8) == 0))
+		{
+		    $output .= "<a href=\"{$gig['url']}\">";
+		}
+		else if (strlen($gig['url']) > 0)
+		{
+			$output .= "<a href=\"http://{$gig['url']}\">";
+		}
+		$output .= stripslashes($gig['title']);
+		if (strlen($gig['url']) > 0)
+		{
+			$output .= "</a>";
+		}
+
+		$output .= "</span> <div class='details'>";
+
+		if (strlen($gig['location']) > 0)
+        	{
+        		$output .= "<p class='location'>" .  stripslashes($gig['location']) . "</p>";
+        	}
+        	if (strlen($gig['description']) > 0)
+        	{
+        		$output .= "<p class='description'>" . stripslashes($gig['description']) . " </p>";
+        	}
+
+		$output .= '</div>';
+		return $output;
 	}
 
 
